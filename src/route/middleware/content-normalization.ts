f8eca90db4e4a90d4eaec2030c325188faa26a96
lightyears <lightyears1998@hotmail.com>
2023-05-12 14:29:38 +0800

    Fix cognitive complexity

--- src/route/middleware/content-normalization.ts
+++ src/route/middleware/content-normalization.ts
@@ -8,34 +8,39 @@ export const contentNormalizationMiddleware: IMiddleware = async (
 ) => {
   const ret = await next();
 
-  if (ctx.body) {
-    if (typeof ctx.body === "object") {
-      if ("errors" in ctx.body || "payload" in ctx.body) {
-        // Normalize errors
-        ctx.body.errors = ctx.body.errors || [];
-        for (let i = 0; i < ctx.body.errors.length; ++i) {
-          if (ctx.body.errors[i] instanceof ServerError) {
-            ctx.body.errors[i] = {
-              name: ctx.body.errors[i].constructor.name,
-              message: ctx.body.errors[i].message,
-            };
-          }
-        }
+  if (ctx.body && typeof ctx.body === "object") {
+    if ("errors" in ctx.body || "payload" in ctx.body) {
+      // Normalize errors
+      ctx.body.errors = normalizeErrors(ctx.body.errors);
 
-        // Normalize payload
-        ctx.body.payload = ctx.body.payload || {};
+      // Normalize payload
+      ctx.body.payload = ctx.body.payload || {};
 
-        // Attach timestamp
-        ctx.body.timestamp = new Date().toISOString();
-      } else {
-        ctx.body = {
-          errors: [],
-          payload: ctx.body,
-          timestamp: new Date().toISOString(),
-        };
-      }
+      // Attach timestamp
+      ctx.body.timestamp = new Date().toISOString();
+    } else {
+      ctx.body = {
+        errors: [],
+        payload: ctx.body,
+        timestamp: new Date().toISOString(),
+      };
     }
   }
 
   return ret;
 };
+
+function normalizeErrors(errors: Error[]): ServerError[] {
+  errors = errors || [];
+
+  for (let i = 0; i < errors.length; ++i) {
+    if (errors[i] instanceof ServerError) {
+      errors[i] = {
+        name: errors[i].constructor.name,
+        message: errors[i].message,
+      };
+    }
+  }
+
+  return errors;
+}
